35ceb2e8e8295be557ba32b488d3102fa140b2ed
Ethan Willers <ethan.willers@oit.edu>
2023-01-29 23:27:42 -0800

    seeds in seeds/seeds.sql complete

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const connection = require("./config/connection.js");
 
 connection.query(
-    '',
+    'SELECT * FROM departments',
+    function(err, results, fields) {
+        console.table(results);
+    }
+);
+
+connection.query(
+    'SELECT * FROM roles JOIN departments ON roles.department_id = departments.id',
     function(err, results, fields) {
         console.table(results);
     }
