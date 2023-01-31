cd54c73e7262a26500f524bc1832192d8cf4d185
Ethan Willers <willers.enw@gmail.com>
2023-01-31 13:06:47 -0800

    added all table querys

--- index.js
+++ index.js
@@ -15,17 +15,15 @@ connection.query(
 );
 
 connection.query(
-    'SELECT * FROM employees JOIN roles ON employees.role_id = roles.id JOIN departments ON roles.department_id = departments.id JOIN employees AS m ON employees.manager_id = m.id',
-    function(err, results, fields) {
-        console.table(results);
-    }
-);
-
-connection.query(
-    `SELECT T1.id, T1.first_name, T1.last_name, T1.role_id, CONCAT(T2.first_name, ' ', T2.last_name) AS manager 
+    `SELECT T1.id, T1.first_name, T1.last_name, roles.title, departments.name AS departments, roles.salary, CONCAT(T2.first_name, ' ', T2.last_name) AS manager 
     FROM employees AS T1
     LEFT JOIN employees AS T2
-    ON T1.id = T2.manager_id`,
+    ON T1.manager_id = T2.id
+    JOIN roles 
+    ON T1.role_id = roles.id
+    JOIN departments 
+    ON roles.department_id = departments.id
+    ORDER BY T1.id`, 
     function(err, results, fields) {
         console.table(results);
     }
